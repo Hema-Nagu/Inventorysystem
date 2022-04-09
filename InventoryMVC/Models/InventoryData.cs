f38716f6f0899957ace2c2d721261ee65f48a23d
Hemalatha N <100828171+Hema-Nagu@users.noreply.github.com>
2022-04-09 20:28:50 +0530

    Add files via upload

---
+++ InventoryMVC/Models/InventoryData.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventoryMVC.Models
+{
+    public class InventoryData
+    {
+        
+        [Display(Name ="Inventory ID")]
+        public int Id { get; set; }
+        [Display(Name ="Product ID")]
+        public int ProductId { get; set; }
+        [Display(Name ="Stocks Available")]
+        public int stock_product_quantity { get; set; }
+    }
+}
---
+++ InventoryMVC/Models/ProductData.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace InventoryMVC.Models
+{
+    public class ProductData
+    {
+        [Display(Name ="Product ID")]
+        public int product_Id { get; set; }
+        [Display(Name ="Product Name")]
+        public string product_Name { get; set; }
+        public int BrandId { get; set; }
+        public int SupplierId { get; set; }
+        public int CategoryId { get; set; }
+        [Display(Name ="Price")]
+        public double product_price { get; set; }
+
+       
+        public virtual InventoryData inventory { get; set; }
+    }
+}
